b985fff4c04d156c01c9cc075cbe8165f7a575dd
WiraJB <wirajulian88@gmail.com>
2020-04-15 03:15:46 +0800

    Mahasiswa bebal

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,22 +2,25 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan=4;
-        switch (jalanJalan){
-            case 1:
-                System.out.println("urang lapas");
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs   : \"Trimakasih pak\"");
+                System.out.println("Dosen : \"Selamat ya !!\"");
                 break;
-            case 2:
-                System.out.println("urang acayan");
+            case'B':
+                System.out.println("Mhs   : \"Kasih A kawa lah pak nanggung banar\"");
+                System.out.println("Dosen : \"Behapal\"");
                 break;
-            case 3:
-                System.out.println("urang nyamuk");
-                break;
-            case 4:
-                System.out.println("dubil dit");
+            case 'C':
+                System.out.println("Mhs   : \"loh knapa C ulun pa\"");
+                System.out.println("Dosen : \"Maaf Anda Kurang Beruntung\"");
                 break;
             default:
-                System.out.println("makhluk halus");
+                System.out.println("Mhs   : \"Haaaaah knapa nilai saya begini...kan saya slalu hadir dan ngrjakan tugas\"");
+                System.out.println("Dosen : \"Oh ya tunggu bentar\"");
+                System.out.println("Dosen : \"*memeriksa\"");
+                System.out.println("Mhs   : \"Tapi Boong....Hiya hiya hiya...*Kabur..\"");
                 break;
         }
     }
